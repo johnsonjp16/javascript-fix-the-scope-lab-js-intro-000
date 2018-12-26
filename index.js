6f16447b04a6b46a575ae1ab068d0d9ff8bce9c6
Jeff Johnson <johnsonjp@yahoo.com>
2018-12-26 21:32:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ function yourAnimal() {
 }
 
 var n = 525
+const two = 2
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
